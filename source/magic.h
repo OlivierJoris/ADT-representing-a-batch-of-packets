57e7ee6a6b8ccff3d53fa5d633c4c22232fe8259
Olivier Joris <joris.olivier@hotmail.com>
2021-03-22 21:50:23 +0100

    MAGICinit implementation

--- source/magic.h
+++ source/magic.h
@@ -1,4 +1,10 @@
+#ifndef __MAGIC__
+#define __MAGIC__
+
+typedef struct MAGIC *MAGIC;
 
 MAGIC MAGICinit(int maxSize, int addrSize);
 int MAGICindex(MAGIC m, char* dest);
 void MAGICreset(MAGIC m);
+
+#endif
