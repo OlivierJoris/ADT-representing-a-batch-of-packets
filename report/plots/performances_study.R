b319bbde48f1b1bd66a5da6017d7ecf7719ffd29
Olivier Joris <joris.olivier@hotmail.com>
2021-04-01 19:05:04 +0200

    correction time + memo

--- report/plots/performances_study.R
+++ report/plots/performances_study.R
@@ -2,17 +2,30 @@ ht_index <- read.csv("~/Documents/Univ/Projets/INFO0027/source/MAGIC1/index.txt"
 ht_reset <- read.csv("~/Documents/Univ/Projets/INFO0027/source/MAGIC1/reset.txt", header=FALSE)
 ht_gen <- ht_index + ht_reset
 
+ht_mem <- read.csv("~/Documents/Univ/Projets/INFO0027/source/MAGIC1/mem.txt", header=FALSE)
+
 tst_index <- read.csv("~/Documents/Univ/Projets/INFO0027/source/MAGIC2/index.txt", header=FALSE)
 tst_reset <- read.csv("~/Documents/Univ/Projets/INFO0027/source/MAGIC2/reset.txt", header=FALSE)
 tst_gen <- tst_index + tst_reset
 
-ht_nb_addresses <- seq(3000, 90000, by=3000)
-tst_nb_adresses <- seq(1000, 30000, by=1000)
+tst_mem <- read.csv("~/Documents/Univ/Projets/INFO0027/source/MAGIC2/mem.txt", header=FALSE)
 
-plot(ht_nb_addresses, ht_gen, type = "l", xlab="Number of addresses", ylab="Time (in seconds)", main="Hash table : total time", ylim=c(0, 0.11), xlim=c(0,90000), pch=15, col="blue")
-plot(ht_nb_addresses, ht_index, type = "l", xlab="Number of addresses", ylab="Time (in seconds)", main="Hash table : MAGICindex time", ylim=c(0, 0.089), xlim=c(0,90000), pch=15, col="blue")
-plot(ht_nb_addresses, ht_reset, type = "l", xlab="Number of addresses", ylab="Time (in seconds)", main="Hash table : MAGICReset time", ylim=c(0, 0.021), xlim=c(0,90000), pch=15, col="blue")
+nb_addresses <- seq(500, 10000, by=500)
 
-plot(tst_nb_adresses , tst_gen, type = "l", xlab="Number of addresses", ylab="Time (in seconds)", main="Ternary search trie : total time", ylim=c(0, 0.067), xlim=c(0,30000), pch=15, col="blue")
-plot(tst_nb_adresses , tst_index, type = "l", xlab="Number of addresses", ylab="Time (in seconds)", main="Ternary search trie : MAGICindex time", ylim=c(0, 0.064), xlim=c(0,30000), pch=15, col="blue")
-plot(tst_nb_adresses , tst_reset, type = "l", xlab="Number of addresses", ylab="Time (in seconds)", main="Ternary search trie : MAGICReset time", ylim=c(0, 0.0025), xlim=c(0,30000), pch=15, col="blue")
+plot(nb_addresses, ht_gen, type = "l", xlab="Number of addresses", ylab="Time (in seconds)", main="Total time", ylim=c(0, max(tst_index) + max(tst_reset)), xlim=c(0,10000), pch=15, col="blue")
+lines(nb_addresses, tst_gen, col = "red")
+legend("topleft", legend = c("Hash table", "Ternary search trie"), col=c("blue", "red"), pch = c(15,15))
+
+plot(nb_addresses, ht_index, type = "l", xlab="Number of addresses", ylab="Time (in seconds)", main="MAGICindex time", ylim=c(0, max(tst_index)), xlim=c(0,10000), pch=15, col="blue")
+lines(nb_addresses, tst_index, col = "red")
+legend("topleft", legend = c("Hash table", "Ternary search trie"), col=c("blue", "red"), pch = c(15,15))
+
+plot(nb_addresses , ht_reset, type = "l", xlab="Number of addresses", ylab="Time (in seconds)", main="MAGICReset time", ylim=c(0, max(tst_reset)), xlim=c(0,10000), pch=15, col="blue")
+lines(nb_addresses , tst_reset,  col = "red")
+legend("topleft", legend = c("Hash table", "Ternary search trie"), col=c("blue", "red"), pch = c(15,15))
+
+plot(nb_addresses , ht_mem, type = "l", xlab="Number of addresses", ylab="Size (in bytes)", main="Memory allocations", ylim=c(0, max(tst_mem)), xlim=c(0,10000), pch=15, col="blue")
+lines(nb_addresses , tst_mem,  col = "red")
+legend("topleft", legend = c("Hash table", "Ternary search trie"), col=c("blue", "red"), pch = c(15,15))
+
+
